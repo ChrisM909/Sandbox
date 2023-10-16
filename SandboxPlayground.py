13097eca13126ad956699b95b13ea2d292165a61
ChrisM90916 <02christopher.melbow@gulllakecs.org>
2023-10-16 08:11:26 -0400

    Add a Print statement

--- SandboxPlayground.py
+++ SandboxPlayground.py
@@ -5,3 +5,4 @@
 print("Hello Christopher Melbow!")
 print("\nI am looking forward to the weekend!")
 print("I am looking forward also to hanging out with my friends for bowling.")
+print("It is a Monday.")
